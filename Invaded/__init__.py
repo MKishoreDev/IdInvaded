ed09acb0e932b4e1aea8ef0dd64d14589a938401
AuraMoon55 <garganshul553@gmail.com>
2022-08-27 20:19:57 +0530

    Init

---
+++ Invaded/__init__.py
@@ -0,0 +1,28 @@
+from pyrogram import Client
+from os import getenv
+from dotenv import load_dotenv
+from pymongo import MongoClient
+
+
+load_dotenv()
+
+API_ID = int(getenv("API_ID"))
+API_HASH = str(getenv("API_HASH"))
+TOKEN = str(getenv("BOT_TOKEN"))
+IS_BOT = bool(getenv("IS_BOT"))
+MONGO_URI = = str(getenv("MONGO_URI"))
+
+if IS_BOT:
+  inv = Client(
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=TOKEN
+  )
+else:
+  inv = Client(
+    api_id=API_ID,
+    api_hash=API_HASH,
+    session_name=TOKEN
+  )
+
+db = MongoClient(MONGO_URI).invaded
