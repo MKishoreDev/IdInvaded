15721d85c8e4738f05cec996aa59e1ce23c2ebd6
Aasf [ありがとう] <89995228+AasfCyberKing@users.noreply.github.com>
2022-08-28 15:53:52 +0530

    Create start.py

---
+++ Invaded/plugins/start.py
@@ -0,0 +1,95 @@
+import time
+import re
+import datetime
+import random
+import requests
+
+from Invaded import inv, invaded_cmd, StartTime
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram import filters, enums
+
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    ping_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+
+    while count < 4:
+        count += 1
+        remainder, result = divmod(seconds, 60) if count < 3 else divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        ping_time += time_list.pop() + ", "
+
+    time_list.reverse()
+    ping_time += ":".join(time_list)
+
+    return ping_time
+
+
+buttons = [
+    [
+        InlineKeyboardButton(
+            text=f"✲ Summon Me ✲", url=f"telegram.me/Kawaii_Robot?startgroup=true")
+    ],
+    [
+        InlineKeyboardButton(text="[► Report Error ◄]", url="https://t.me/Aasf_Cyberking"),
+        InlineKeyboardButton(text="[► Get Updates ◄]", url="https://t.me/CityOfCreations"),
+    ],
+    [
+        InlineKeyboardButton(text="⊵ Help Guidelines ⊴", callback_data="help_enter"),
+    ],
+]
+
+
+PM_START_TEXT = """
+<b>Whassup</b> {}, `I'm An Anime Boy Themed Group Management Bot!`
+➖➖➖➖➖➖➖➖➖➖➖➖➖
+• `Alive Time:` `{}`
+➖➖➖➖➖➖➖➖➖➖➖➖➖
+<b>Keep Your Group Secure From Spammers by Adding me! ××</b>
+"""
+
+GROUP_START_TEXT = """
+<b>Hoi {}, I'm Alive In {}</b>
+<b>From:</b> `{}`
+"""
+
+@inv.on_message(invaded_cmd("start")
+async def test(_, m: Message):
+    if m.chat.type == enums.ChatType.PRIVATE:
+        uptime = get_readable_time((time.time() - StartTime))
+        kk = await m.reply(text="`Analyzing The User`")
+        time.sleep(2)
+        mm = await kk.edit_text("`...`")
+        time.sleep(2)
+        ll = await mm.edit_text("`Processing...`")
+        time.sleep(3)
+        await ll.delete()
+        await m.reply_video(
+            "https://telegra.ph/file/b011b50739d2f02a82ed6.mp4",
+            caption=PM_START_TEXT.format(
+                m.from_user.mention, uptime
+            ),
+            reply_markup=InlineKeyboardMarkup(buttons),
+        )
+    if not m.chat.type == enums.ChatType.PRIVATE:
+        uptime = get_readable_time((time.time() - StartTime))
+        kk = await m.reply(text="`Analyzing The User...`")
+        time.sleep(2)
+        await kk.delete()
+        await m.reply_video(
+            "https://telegra.ph/file/ebc3237529228cb87b99f.mp4",
+            caption=GROUP_START_TEXT.format(
+                m.from_user.mention,
+                m.chat.title,
+                uptime,
+                reply_markup=InlineKeyboardMarkup(buttons),
+            ),
+        )
