d992537d019171eab714363af0824e00239b452f
Flora 흥분한 <106656839+HornyFlora@users.noreply.github.com>
2022-08-28 14:36:37 +0530

    Update __init__.py

--- Invaded/__init__.py
+++ Invaded/__init__.py
@@ -21,6 +21,7 @@ MONGO_DB_URL = str("mongodb+srv://AasfCyberKing:Mm11$$$$@invaded.exzjpln.mongodb
 
 if IS_BOT:
   inv = Client(
+    name="Invaded",
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=TOKEN
