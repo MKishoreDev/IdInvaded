01f9b6a169fbed9a28665e4b5fa1206ffce602c8
Flora 흥분한 <106656839+HornyFlora@users.noreply.github.com>
2022-08-30 20:09:54 +0530

    Update helpbutton.py

--- Invaded/plugins/helpbutton.py
+++ Invaded/plugins/helpbutton.py
@@ -1,2 +1,11 @@
-Whois = "Whois"
-      
+from Invaded import inv_modules
+
+inv_modules.append({"Module_Name": "whois"})
+
+class Help_Text:
+    helpp = {
+        "whois_help": """
+• /whois - To check info
+
+"""
+    }
