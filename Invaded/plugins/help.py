a0be26dd96f89419e12a8241d21195b900bc289a
Ryu_sama <90622022+Ryu120@users.noreply.github.com>
2022-08-30 06:02:59 -0700

    typo

--- Invaded/plugins/help.py
+++ Invaded/plugins/help.py
@@ -11,9 +11,6 @@ Report issues @idk
 @inv.on_message(invaded_cmd('help'))
 def invhelp(_,message):
 
-        inv.send_message(message.chat.id , HELP_STRING , reply_markup=InlineKeyboardMarkup(keyboard))
-
-    else:
         inv.send_photo(message.chat.id , "https://telegra.ph/file/cd1611c22cc9ad650e0de.jpg" ,  caption=HELP_STRING , reply_markup=InlineKeyboardMarkup(
             [
                 [InlineKeyboardButton("Pm me for more details"  , url="t.me/Invaded_Robot?start=help")]
