b6b132bc5a170cb167de7c8015e1084926006f2b
Aasf [ありがとう] <89995228+AasfCyberKing@users.noreply.github.com>
2022-08-30 15:17:35 +0530

    Update whois.py

--- Invaded/plugins/whois.py
+++ Invaded/plugins/whois.py
@@ -2,7 +2,7 @@ from Invaded import inv, invaded_cmd
 from pyrogram import filters
 from pyrogram.types import Message
 
-@inv.on_message(invaded_cmd("(info|whois) "))
+@inv.on_message(invaded_cmd("whois"))
 async def whois(_, m: Message):
     if m.reply_to_message:
         user = m.reply_to_message.from_user.id
@@ -17,7 +17,19 @@ async def whois(_, m: Message):
     except Exception as e:
         await m.reply_photo("https://c4.wallpaperflare.com/wallpaper/976/117/318/anime-girls-404-not-found-glowing-eyes-girls-frontline-wallpaper-preview.jpg", caption=f"`404 Error Occurred Failed To Get Data Of The User`\n\n `{e}`")
         return
-    await inv.send_message(
-        m.chat.id,
-        f"**╒═══「 Appraisal Results: 」**\n**➛ First Name:** `{data.first_name}`\n**➛ Last Name:** `{data.last_name}`\n**➛ Username: @{data.username}**\n**➛ Userlink: {data.mention}**\n**➛ User ID:** `{data.id}`\n**➛ About:** `{boom.bio}`",
-    )
+   if data.photo:
+     try:
+        pic = await inv.download_media(data.photo.big_file_id)
+        await inv.send_photo(
+            m.chat.id,
+            pic,
+            caption=f"**╒═══「 Appraisal Results: 」**\n**➛ First Name:** `{data.first_name}`\n**➛ Last Name:** `{data.last_name}`\n**➛ Username: @{data.username}**\n**➛ Userlink: {data.mention}**\n**➛ User ID:** `{data.id}`\n**➛ About:** `{boom.bio}`",
+        )
+     except Exception as e:
+         await m.reply_photo("https://c4.wallpaperflare.com/wallpaper/976/117/318/anime-girls-404-not-found-glowing-eyes-girls-frontline-wallpaper-preview.jpg", caption=f"`404 Error Occurred Failed To Get Data Of The User`\n\n `{e}`")
+         return
+    else:
+        await inv.send_message(
+            m.chat.id,
+            f"**╒═══「 Appraisal Results: 」**\n**➛ First Name:** `{data.first_name}`\n**➛ Last Name:** `{data.last_name}`\n**➛ Username: @{data.username}**\n**➛ Userlink: {data.mention}**\n**➛ User ID:** `{data.id}`\n**➛ About:** `{boom.bio}`",
+        )
