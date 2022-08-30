a54287b0d9bab7f6a02d5de9a0e31e9f7e0696d6
Ryu_sama <90622022+Ryu120@users.noreply.github.com>
2022-08-30 05:43:36 -0700

    the end

---
+++ Invaded/plugins/modules.py
@@ -0,0 +1,12 @@
+from Invaded import inv_modules
+
+inv_modules.append({"Module_Name": "whois"})
+
+class Help_Text:
+    helpp = {
+        "whois_help": """
+• /whois - To check info
+
+"""
+    }
+      
