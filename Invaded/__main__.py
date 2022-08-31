cb71ac919822cc899ea1d061036d6c88dd81a7cb
Aasf [ありがとう] <89995228+AasfCyberKing@users.noreply.github.com>
2022-08-31 13:16:40 +0530

    Update __main__.py

--- Invaded/__main__.py
+++ Invaded/__main__.py
@@ -1,6 +1,22 @@
+import logging
+import importlib
+
 from Invaded import inv
+from Invaded.plugins import ALL_MODULES
 from pyrogram import idle
 
 
 if __name__ == "__main__":
+    logging.basicConfig(
+        handlers=[logging.FileHandler("logs.txt"), logging.StreamHandler()],
+        level=logging.INFO,
+        format=FORMAT,
+        datefmt="[%X]",
+    )
+    logging.getLogger("pyrogram").setLevel(logging.INFO)
+    for module in ALL_MODULES:
+      importlib.import_module("Invaded.plugins." + module)
+    
+    print("Project By AuraMoon55 | Ryu120 | AasfCyberKing")
+ #   inv.send(LOG_GROUP_ID, "I Am Online")
     idle()
