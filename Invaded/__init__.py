6698f9a0246ca393d1c256513eb3c2ca1d94f473
Aasf [ありがとう] <89995228+AasfCyberKing@users.noreply.github.com>
2022-08-31 13:05:22 +0530

    Update __init__.py

--- Invaded/__init__.py
+++ Invaded/__init__.py
@@ -1,9 +1,5 @@
-import glob
 import time
-import importlib
 import logging
-import sys
-from pathlib import Path
 from pyrogram import Client, filters
 from os import getenv
 from dotenv import load_dotenv
@@ -64,27 +60,5 @@ UBOT_ID = y.id
 db = MongoClient(MONGO_DB_URL).invaded
 StartTime = time.time()
 
-def load_plugins(plugin_name):
-    path = Path(f"Invaded/plugins/{plugin_name}.py")
-    name = "Invaded.plugins.{}".format(plugin_name)
-    spec = importlib.util.spec_from_file_location(name, path)
-    load = importlib.util.module_from_spec(spec)
-    load.logger = logging.getLogger(plugin_name)
-    spec.loader.exec_module(load)
-    sys.modules["Invaded.plugins." + plugin_name] = load
-    print("Imported --> " + plugin_name)
-
 def invaded_cmd(com):
   return filters.command([com, f"{com}@{BOT_USERNAME}"], prefixes=["?","$","!","/",".","inv ","invades ","Inv ","Invaded "])
-
-
-
-path = "Invaded/plugins/*.py"
-files = glob.glob(path)
-for name in files:
-    with open(name) as a:
-        thepath = Path(a.name)
-        plugin_name = thepath.stem
-        load_plugins(plugin_name.replace(".py", ""))
-
-
