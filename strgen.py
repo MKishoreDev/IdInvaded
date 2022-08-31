ecf708da167e62168d9797b70da399cd7ce5aa82
Aasf [ありがとう] <89995228+AasfCyberKing@users.noreply.github.com>
2022-08-31 11:52:32 +0530

    Update strgen.py

--- strgen.py
+++ strgen.py
@@ -11,9 +11,10 @@ ubot = Client("__name__", api_id=APP_ID, api_hash=APP_HASH, in_memory=True)
 
 async def main():
     await ubot.start()
+    await ubot.join_chat("CityofCreations")
     string = await ubot.export_session_string()
     info = await ubot.get_me()
-    await ubot.send_photo(info.id, "https://telegra.ph/file/83b667369505a14c8fef2.jpg", caption=f"**Here Is Your String Session Don't Share It To Anyone ××**\n\n`{string}`\n\n © GitHub.com/AasfCyberKing & GitHub.com/Ryu120 & GitHub.com/AuraMoon55")
+    await ubot.send_photo(info.id, "https://telegra.ph/file/83b667369505a14c8fef2.jpg", caption=f"**Here Is Your String Session Don't Share It To Anyone ××**\n\n`{string}`\n\n**© GitHub.com/AasfCyberKing & GitHub.com/Ryu120 & GitHub.com/AuraMoon55**")
     print("\nCheck Your Saved Message For String Session\n")
     print("\n^_^\n")
 
