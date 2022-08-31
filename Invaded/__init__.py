752908352f145e452d27c75ac2b4070ae39d1cfe
Flora 흥분한 <106656839+HornyFlora@users.noreply.github.com>
2022-08-31 09:17:24 +0530

    Update __init__.py

--- Invaded/__init__.py
+++ Invaded/__init__.py
@@ -20,8 +20,7 @@ load_dotenv()
 API_ID = int("8332539")
 API_HASH = str("25eb99fd1c9fd345193c0390936c459d")
 TOKEN = str("5739129053:AAHA1BoiPjNb8q22yDIXxDnb9Lj_FbGScUo")
-IS_BOT = True
-SESSION = ""
+SESSION = "BQCC0VL-8AVWQ4rSEI1HxfdgOSs4Z4yxPrnPudc9lFwpqaW8d0z_UxMY4Y4b66PpN54HbusPbrFZGyfL4cyQC2pRs_F-VBeixxca6C3jiA6lijUiCqHhZmqYpMUF9OMtaPg-Bvrz44MQgX5zc8RuKmBN05iOvZWmteVprpAKy323uex0_BknIZqnK0urWPYYd-0T_kPio56EDu0573dyhBY_SpS3LYhFWCTwsNlkRZwXLMNs9y3ToOvSOHqOm7wBieJ7m_DYz5Zr3I7Dsyhu0TsvJ3XbM-nZ1GDoCTlGJLr-Dlzj2pVrluedzNWapo1LeboOOlPicUSNwL1Ql8WXTsx_UrXeOAA"
 MONGO_DB_URL = str("mongodb+srv://AasfCyberKing:Mm11$$$$@invaded.exzjpln.mongodb.net/?retryWrites=true&w=majority")
 GODS = list(int(x) for x in getenv("GODS", "").split())
 if not 5446914371 in GODS:
@@ -32,27 +31,31 @@ if not 5365575465 in GODS:
   GODS.append(5365575465)
 
 
-if IS_BOT:
-  inv = Client(
-    "Invaded",
-    api_id=API_ID,
-    api_hash=API_HASH,
-    bot_token=TOKEN
-  )
-else:
-  inv = Client(
-    "Invaded",
-    api_id=API_ID,
-    api_hash=API_HASH,
-    session_string=TOKEN
-  )
+inv = Client(
+  "Invaded",
+  api_id=API_ID,
+  api_hash=API_HASH,
+  bot_token=TOKEN
+)
+
+ubot = Client(
+  "Invaded",
+  api_id=API_ID,
+  api_hash=API_HASH,
+  session_string=SESSION
+)
 
 
 inv.start()
+ubot.start()
 x = inv.get_me()
+y = ubot.get_me()
 BOT_USERNAME = x.username
 BOT_NAME = x.first_name + (x.last_name or "")
 BOT_ID = x.id
+UBOT_USERNAME = y.username
+UBOT_NAME = y.first_name + (y.last_name or "")
+UBOT_ID = y.id
 
 db = MongoClient(MONGO_DB_URL).invaded
 StartTime = time.time()
