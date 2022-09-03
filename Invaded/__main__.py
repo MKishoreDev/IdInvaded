30a5ee2c6e93644cfd42c478818ecdda468b7949
Aasf [ありがとう] <89995228+AasfCyberKing@users.noreply.github.com>
2022-09-03 18:44:35 +0530

    Update __main__.py

--- Invaded/__main__.py
+++ Invaded/__main__.py
@@ -34,6 +34,7 @@ async def main():
  logging.getLogger("pyrogram").setLevel(logging.INFO)
  for module in ALL_MODULES:
    importlib.import_module("Invaded.plugins." + module) 
+   log.info("Successfully Imported Plugins:" + mofule)
  log.info("Project By AuraMoon55 | Ryu120 | AasfCyberKing")
 
 if __name__ == "__main__":
