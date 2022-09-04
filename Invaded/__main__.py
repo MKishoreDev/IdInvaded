cf4c15c42ea7e0a70e548e9c644f3f9c3a17916f
Aasf [ありがとう] <89995228+AasfCyberKing@users.noreply.github.com>
2022-09-04 16:35:42 +0530

    Update __main__.py

--- Invaded/__main__.py
+++ Invaded/__main__.py
@@ -1,4 +1,5 @@
 import logging
+import glob
 import sys
 import time
 import asyncio
@@ -8,6 +9,7 @@ import datetime
 from Invaded import inv, log, BOT_NAME, UBOT_NAME, LOG_GROUP_ID
 from Invaded.plugins import ALL_MODULES
 from pyrogram import idle, __version__
+from pathlib import Path
 
 now = datetime.datetime.now()
 t = time.localtime()
@@ -23,6 +25,28 @@ Timestamp: {ts}"""
 
 FORMAT = "[INFO] %(message)s"
 
+def load_plugins(plugin_name):
+    root = "Invaded/plugins/*.py"
+    total = f"{len(glob.glob(root))}"
+    path = Path(f"Invaded/plugins/{plugin_name}.py")
+    name = "Invaded.plugins.{}".format(plugin_name)
+    spec = importlib.util.spec_from_file_location(name, path)
+    load = importlib.util.module_from_spec(spec)
+    load.logger = logging.getLogger(plugin_name)
+    spec.loader.exec_module(load)
+    sys.modules["Invaded.plugins." + plugin_name] = load
+    print("Total Plugins -->" + toral)
+    print("Imported --> " + plugin_name)
+
+path = "Invaded/plugins/*.py"
+files = glob.glob(path)
+for name in files:
+    with open(name) as a:
+        thepath = Path(a.name)
+        plugin_name = thepath.stem
+        names = plugin_name.replace(".py", "")
+        load_plugins(names)
+
 async def main():
  logging.basicConfig(
      handlers=[logging.FileHandler("logs.txt"), logging.StreamHandler()],
@@ -32,9 +56,6 @@ async def main():
   )
  log.info(sys.version + "\n\n" +  (boot_msg % (now.strftime("%A"))))
  logging.getLogger("pyrogram").setLevel(logging.INFO)
- for module in ALL_MODULES:
-   importlib.import_module("Invaded.plugins." + module) 
-   log.info("Successfully Imported Plugins:" + module)
  log.info("Project By AuraMoon55 | Ryu120 | AasfCyberKing")
  
 if __name__ == "__main__":
